fd3d0bef6eb00a29495e97d5d90fb2e4e86b366e
Tyler Gu <jiaweig3@illinois.edu>
2025-03-16 17:30:32 -0500

    Avoid auto retry on MongoDB client
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- mongodb/writer/writer.go
+++ mongodb/writer/writer.go
@@ -86,7 +86,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx,
-		options.Client().ApplyURI(getDSN()))
+		options.Client().ApplyURI(getDSN()).SetRetryWrites(false))
 	if err != nil {
 		panic(err)
 	}
